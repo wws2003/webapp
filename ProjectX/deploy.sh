268e24819f4d8e6d7b67148c08e9b6af4b7b87da
trungpt8 <trungpt8@fsoft.com.vn>
2014-12-09 01:25:26 +0900

    Remove not neccessary items

--- ProjectX/deploy.sh
+++
@@ -1,6 +0,0 @@
-export CATALINA_HOME=/Users/wws2003/servers/apache-tomcat-6.0.41
-rm -rf $CATALINA_HOME/webapps/autospring*
-echo "Removed webapp folder in catalina home!"
-mvn2 package -Dmaven.test.skip=true
-cp target/autospring.war $CATALINA_HOME/webapps/
-echo "Copied artifact to catalina home!"
