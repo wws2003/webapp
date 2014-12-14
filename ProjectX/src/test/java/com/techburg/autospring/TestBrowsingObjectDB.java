cbbbdbd09cc13ef91ef20a0ab1173f9720c98f0c
trungpt8 <trungpt8@fsoft.com.vn>
2014-12-14 19:07:29 +0900

    Changes in lock mechanism to update DB of browsing objects

---
+++ ProjectX/src/test/java/com/techburg/autospring/TestBrowsingObjectDB.java
@@ -0,0 +1,68 @@
+package com.techburg.autospring;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.techburg.autospring.db.task.abstr.IDBTaskExecutor;
+import com.techburg.autospring.factory.abstr.IWorkspaceFactory;
+import com.techburg.autospring.model.business.BrowsingObject;
+import com.techburg.autospring.model.business.Workspace;
+import com.techburg.autospring.service.abstr.IBrowsingObjectPersistentService;
+import com.techburg.autospring.service.abstr.IWorkspacePersistenceService;
+
+public class TestBrowsingObjectDB {
+
+	//private EntityManagerFactory mEntityManagerFactory;
+	private IBrowsingObjectPersistentService mBrowsingObjectService;
+	private IWorkspaceFactory mWorkspaceFactory;
+	private IWorkspacePersistenceService mWorkspacePersistenceService;
+	private IDBTaskExecutor mTaskExecutor;
+	
+	@Before
+	public void setUp() throws Exception {
+		//Retrieve mPersistenceService by factory method
+		String xmlPath = "file:src/main/webapp/WEB-INF/spring-conf/springmvc-conf.xml";
+		try {
+			ApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlPath);
+			//applicationContext is also a bean factory...
+			//mEntityManagerFactory = applicationContext.getBean("entityManagerFactory", EntityManagerFactory.class);
+			mBrowsingObjectService = applicationContext.getBean("browsingObjectPersistentService", IBrowsingObjectPersistentService.class);
+			mWorkspaceFactory = applicationContext.getBean("workspaceFactory", IWorkspaceFactory.class);
+			mTaskExecutor = applicationContext.getBean("dBTaskExecutor", IDBTaskExecutor.class);
+			mWorkspacePersistenceService = applicationContext.getBean("workspacePersistenceService", IWorkspacePersistenceService.class);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		BrowsingObject browsingObject = mBrowsingObjectService.getBrowsingObjectById(1);
+		assertNotNull(browsingObject);
+		List<BrowsingObject> childBrowsingObjects = new ArrayList<BrowsingObject>();
+		mBrowsingObjectService.getChildBrowsingObjects(browsingObject, childBrowsingObjects);
+		assertTrue(childBrowsingObjects.size() > 0);
+		
+		//Test new workspace
+		Workspace newWorkspace = mWorkspaceFactory.createWorkspace("workspace5", "script.sh");
+		mWorkspacePersistenceService.persistWorkspace(newWorkspace);
+		
+		//mTaskExecutor.waitAllFinish();
+	}
+
+}
---
+++ ProjectX/target/autospring/WEB-INF/views/workspace.jsp
@@ -0,0 +1,31 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<html>
+<head>
+<link rel="stylesheet" type="text/css"
+	href="<c:url value='/resources/css/common.css'></c:url>"></link>
+</head>
+<style type="text/css">
+input, textarea  {
+	margin-bottom: 15px;
+}
+</style>
+<body>
+	<div class="container">
+		<div class="sidebar"></div>
+		<div class="container_header">Simplest CI !</div>
+		<div class="main_content">
+			<form action=<c:url value="/workspace/new"></c:url> method="POST">
+				Workspace name: <input name="workspacename" type="text"><br>
+				Build script name: <input name="buildscriptname" type="text"><br>
+				Script content: <br>
+				<textarea rows="25" cols="80" name="buildscriptcontent">
+				</textarea>
+				<br> <input type="submit" value="Submit"> <br>
+			</form>
+		</div>
+		<div class="container-footer">
+			<a href='<c:url value="/home"></c:url>'>To home page</a>
+		</div>
+	</div>
+</body>
+</html>
