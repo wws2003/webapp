5f1ea477273ffb98b6579f51af7a70451ee61afe
wws2003 <wws2003@gmail.com>
2016-03-05 00:32:03 +0700

    Fix sqlite concurent bug

--- ProjectX/src/main/java/com/techburg/autospring/service/impl/BrowsingObjectPersistentJPAImpl.java
+++ ProjectX/src/main/java/com/techburg/autospring/service/impl/BrowsingObjectPersistentJPAImpl.java
@@ -10,7 +10,6 @@ import com.techburg.autospring.bo.abstr.IBrowsingObjectBo;
 import com.techburg.autospring.db.task.abstr.AbstractDBTask;
 import com.techburg.autospring.db.task.abstr.IDBTaskExecutor;
 import com.techburg.autospring.db.task.impl.BrowsingObjectDBTaskImpl;
-import com.techburg.autospring.db.task.impl.NoLockingDBTaskExecutorImpl;
 import com.techburg.autospring.model.business.BrowsingObject;
 import com.techburg.autospring.service.abstr.IBrowsingObjectPersistentService;
 import com.techburg.autospring.service.abstr.IBrowsingServiceDelegate;
@@ -21,10 +20,17 @@ public class BrowsingObjectPersistentJPAImpl implements IBrowsingObjectPersisten
 	private IBrowsingObjectBo mBrowsingObjectBo = null;
 	private IDBTaskExecutor mDBTaskExecutor = null;
 	private IBrowsingServiceDelegate mBrowsingServiceDelegate = null; 
+	private int mScheduleMode = -1;
+	
+	public BrowsingObjectPersistentJPAImpl(EntityManagerFactory entityManagerFactory, int scheduleMode) {
+		mEntityManagerFactory = entityManagerFactory;
+		mScheduleMode = scheduleMode;
+	}
 	
 	@Autowired
 	public BrowsingObjectPersistentJPAImpl(EntityManagerFactory entityManagerFactory) {
 		mEntityManagerFactory = entityManagerFactory;
+		mScheduleMode = AbstractDBTask.SCHEDULE_SYNC_MODE;
 	}
 
 	@Autowired
@@ -46,7 +52,7 @@ public class BrowsingObjectPersistentJPAImpl implements IBrowsingObjectPersisten
 	public int persistBrowsingObject(BrowsingObject browsingObject) {
 		BrowsingObjectDBTaskImpl persistTask = new BrowsingObjectDBTaskImpl(mBrowsingObjectBo, mEntityManagerFactory);
 		persistTask.setPersistParams(browsingObject);
-		persistTask.setScheduleMode(AbstractDBTask.SCHEDULE_SYNC_MODE);
+		persistTask.setScheduleMode(mScheduleMode);
 		mDBTaskExecutor.executeDBTask(persistTask);
 		return persistTask.getPersistResult();
 	}
@@ -55,7 +61,7 @@ public class BrowsingObjectPersistentJPAImpl implements IBrowsingObjectPersisten
 	public BrowsingObject getBrowsingObjectById(long id) {
 		BrowsingObjectDBTaskImpl findTask = new BrowsingObjectDBTaskImpl(mBrowsingObjectBo, mEntityManagerFactory);
 		findTask.setGetByIdParam(id);
-		findTask.setScheduleMode(AbstractDBTask.SCHEDULE_SYNC_MODE);
+		findTask.setScheduleMode(mScheduleMode);
 		mDBTaskExecutor.executeDBTask(findTask);
 		return findTask.getGetByIdResult();
 	}
@@ -64,7 +70,7 @@ public class BrowsingObjectPersistentJPAImpl implements IBrowsingObjectPersisten
 	public BrowsingObject getBrowsingObjectByPath(String path) {
 		BrowsingObjectDBTaskImpl findTask = new BrowsingObjectDBTaskImpl(mBrowsingObjectBo, mEntityManagerFactory);
 		findTask.setGetByPathParam(path);
-		findTask.setScheduleMode(AbstractDBTask.SCHEDULE_SYNC_MODE);
+		findTask.setScheduleMode(mScheduleMode);
 		mDBTaskExecutor.executeDBTask(findTask);
 		return findTask.getGetByPathResult();
 	}
@@ -73,7 +79,7 @@ public class BrowsingObjectPersistentJPAImpl implements IBrowsingObjectPersisten
 	public int getChildBrowsingObjects(BrowsingObject parent, List<BrowsingObject> children) {
 		BrowsingObjectDBTaskImpl getChildrenTask = new BrowsingObjectDBTaskImpl(mBrowsingObjectBo, mEntityManagerFactory);
 		getChildrenTask.setGetChildrenParam(parent);
-		getChildrenTask.setScheduleMode(AbstractDBTask.SCHEDULE_SYNC_MODE);
+		getChildrenTask.setScheduleMode(mScheduleMode);
 		mDBTaskExecutor.executeDBTask(getChildrenTask);
 		return getChildrenTask.getGetChildrenResult(children);
 	}
@@ -82,34 +88,34 @@ public class BrowsingObjectPersistentJPAImpl implements IBrowsingObjectPersisten
 	public int removeAllBrowsingObject() {
 		BrowsingObjectDBTaskImpl removeAllTask = new BrowsingObjectDBTaskImpl(mBrowsingObjectBo, mEntityManagerFactory);
 		removeAllTask.setRemoveAllParam();
-		removeAllTask.setScheduleMode(AbstractDBTask.SCHEDULE_SYNC_MODE);
+		removeAllTask.setScheduleMode(mScheduleMode);
 		mDBTaskExecutor.executeDBTask(removeAllTask);
 		return removeAllTask.getRemoveAllResult();
 	}
 
 	@Override
 	public int persistBrowsingObjectInDirectory(String directoryPath, boolean isSyncMode) {
-		return mBrowsingServiceDelegate.persistBrowsingObjectInDirectory(directoryPath, isSyncMode ? nonblokingPrototype() : this);
+		return mBrowsingServiceDelegate.persistBrowsingObjectInDirectory(directoryPath, isSyncMode ? this : getAsyncPersistentService());
 	}
 
 	@Override
 	public int removeBrowsingObjectInDirectory(String directoryPath, boolean isSyncMode) {
-		return mBrowsingServiceDelegate.removeBrowsingObjectInDirectory(directoryPath, isSyncMode ? nonblokingPrototype() : this);
+		return mBrowsingServiceDelegate.removeBrowsingObjectInDirectory(directoryPath, isSyncMode ? this: getAsyncPersistentService());
 	}
 
 	@Override
 	public int removeBrowsingObjectById(long id) {
 		BrowsingObjectDBTaskImpl removeTask = new BrowsingObjectDBTaskImpl(mBrowsingObjectBo, mEntityManagerFactory);
 		removeTask.setRemoveByIdParam(id);
-		removeTask.setScheduleMode(AbstractDBTask.SCHEDULE_SYNC_MODE);
+		removeTask.setScheduleMode(mScheduleMode);
 		mDBTaskExecutor.executeDBTask(removeTask);
 		return removeTask.getRemoveByIdResult();
 	}
 	
-	private IBrowsingObjectPersistentService nonblokingPrototype() {
-		BrowsingObjectPersistentJPAImpl instance = new BrowsingObjectPersistentJPAImpl(mEntityManagerFactory);
+	private IBrowsingObjectPersistentService getAsyncPersistentService() {
+		BrowsingObjectPersistentJPAImpl instance = new BrowsingObjectPersistentJPAImpl(mEntityManagerFactory, AbstractDBTask.SCHEDULE_ASYNC_MODE);
 		instance.setBuildInfoBo(mBrowsingObjectBo);
-		instance.setDBTaskExecutor(new NoLockingDBTaskExecutorImpl());
+		instance.setDBTaskExecutor(mDBTaskExecutor);
 		return instance;
 	}
 
